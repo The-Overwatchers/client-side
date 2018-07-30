df21e47f394af7c6da0ed597ab26628736e6df6d
MAX MCFARLAND <mmcfarland522@gmail.com>
2018-07-30 13:51:41 -0700

    All html is linked with no errors, need form html for debugging

--- scripts/controller/routes.js
+++ scripts/controller/routes.js
@@ -1,1 +1,8 @@
 'use strict'
+
+
+page('/', ctx => app.indexView.init(ctx));
+// --------------------------------------------
+
+// Vinicio - this line has to be placed at the end of the file, AFTER you define any front-end routes
+page(); // Vinicio - this line is starting the Page JS Engine
