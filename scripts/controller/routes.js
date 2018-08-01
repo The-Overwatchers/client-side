ed1b1b3121634f3b3df4d86fb02ae1f4f2f16a0e
Ben Harris <ben@harrishills.com>
2018-08-01 13:39:14 -0700

    Adding login logic

--- scripts/controller/routes.js
+++ scripts/controller/routes.js
@@ -3,6 +3,7 @@
 page('/', ctx => app.indexView.init(ctx));
 page('/games/:id', ctx => app.detailsView.init(ctx));
 page('/login', ctx => app.loginView.init(ctx));
+page('/logout', ctx => app.loginView.logout(ctx));
 
 // Vinicio - this line has to be placed at the end of the file, AFTER you define any front-end routes
 page(); // Vinicio - this line is starting the Page JS Engine
