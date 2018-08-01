888115d2884e99ac13847d1cdca687ec5b2d00e0
Ben Harris <ben@harrishills.com>
2018-08-01 11:42:38 -0700

    Adding user registration and login code to repo

--- scripts/models/users.js
+++ scripts/models/users.js
@@ -6,16 +6,22 @@ var app = app || {};
   let Users = {};
 
   Users.registerUser = (newName) => {
-   $.get('/api/v1/user/register', newName)
-    .then(results => console.log(results)) // TODO: call login function
-    .catch(error => console.error(error));
+    $.get(`${app.ENVIRONMENT.apiUrl}/api/v1/user/register/${newName}`)
+      .then(results => {
+        if(!!results.success) { // eslint-disable-line
+          return app.loginView.regAlert(results.string);
+        } else {
+          return app.loginView.regAlert(results.string);
+        }
+      })
+      .catch(error => console.error(error));
   };
 
   Users.loginUser = (userName) => {
-    $.get('/api/v1/user/login', userName)
-      .then() // append username to local storage
+    $.get(`${app.ENVIRONMENT.apiUrl}/api/v1/user/login/${userName}`)
+      .then(result => console.log(result)) // append username to local storage
       .catch(error => console.error(error));
   };
-  
+
   module.Users = Users;
 })(app);
