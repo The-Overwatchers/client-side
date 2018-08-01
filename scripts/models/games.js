ef1c1e1c8601cbae0471247cb3a48ac15748ba64
Ben Harris <ben@harrishills.com>
2018-08-01 14:55:00 -0700

    added ID to template

--- scripts/models/games.js
+++ scripts/models/games.js
@@ -12,6 +12,15 @@ var app = app || {};
       .catch(error => console.error(error));
   }
 
+  Games.favOneGame = (context, callback) => {
+    $.post(`${app.ENVIRONMENT.apiUrl}/api/v1/favorites/`, {
+      name: context.name,
+      igdb_id: context.id,
+      user: context.user
+    })
+      .then(result => callback(result))
+      .catch(error => console.error(error));
+  }
 
   module.Games = Games;
 })(app);
